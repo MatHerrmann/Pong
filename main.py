afed3577fe3e88119797e05e29f84051a71d0330
Matthias <mherrmann021090@gmail.com>
2022-07-05 14:49:31 +0200

    Minor refactoring + started working on game over feature

--- main.py
+++ main.py
@@ -8,6 +8,17 @@ WIDTH, HEIGHT = 900,600
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 VY = 5
 VX = 5
+GAME_OVER_EVENT = pygame.USEREVENT + 1
+
+class Ball(pygame.Rect):
+  def __init__(self, left, top, width, height, color):
+    super().__init__(left, top, width, height)
+    self.color = color
+
+class Paddle(pygame.Rect):
+  def __init__(self, left, top, width, height, color):
+    super().__init__(left, top, width, height)
+    self.color = color
 
 # Left paddle
 def handle_mov_player_left(keys_pressed, player_L):
@@ -28,8 +39,8 @@ def handle_mov_ball(ball, player_L, player_R):
   global ball_VX, ball_VY
   if ball.x + ball.width + ball_VX > WIDTH or ball.x + ball_VX < 0:
     # Game lost
-    print('Game lost')
-    sys.exit()
+    pygame.event.post(pygame.event.Event(GAME_OVER_EVENT))
+
 
   # Rebouncing with player right
   if ball.colliderect(player_R):
@@ -55,30 +66,37 @@ def handle_mov_ball(ball, player_L, player_R):
 
 def draw(ball, player_L, player_R):
   WIN.fill(color_constants.BLACK)
-  pygame.draw.rect(WIN, color_constants.WHITE, ball)
-  pygame.draw.rect(WIN, color_constants.WHITE, player_L)
-  pygame.draw.rect(WIN, color_constants.WHITE, player_R)
+  pygame.draw.rect(WIN, ball.color, ball)
+  pygame.draw.rect(WIN, player_L.color, player_L)
+  pygame.draw.rect(WIN, player_R.color, player_R)
+
+def initialize():
+  BALL_LENGTH=20
+  BALL_START_X, BALL_START_Y = (WIN.get_width()- BALL_LENGTH) //2, (WIN.get_height() - BALL_LENGTH) // 2
+  PLAYER_WIDTH, PLAYER_HEIGHT = 50,90
+  PLAYER_START_X, PLAYER_START_Y =  20, (WIN.get_height()-PLAYER_HEIGHT)//2
+  ball = Ball(BALL_START_X, BALL_START_Y, BALL_LENGTH, BALL_LENGTH, color_constants.WHITE)
+  player_L = Paddle(PLAYER_START_X, PLAYER_START_Y, PLAYER_WIDTH, PLAYER_HEIGHT, color_constants.WHITE)
+  player_R = Paddle(WIN.get_width() - PLAYER_START_X - PLAYER_WIDTH, PLAYER_START_Y, PLAYER_WIDTH, PLAYER_HEIGHT, color_constants.WHITE)
+  return ball, player_L, player_R
 
 
 def main():
   pygame.display.set_caption('Pong')
   clock = pygame.time.Clock()
-
+  game_started=False
   running = True
   FPS=60
-  BALL_LENGTH=20
-  BALL_START_X, BALL_START_Y = (WIN.get_width()- BALL_LENGTH) //2, (WIN.get_height() - BALL_LENGTH) // 2
-  PLAYER_WIDTH, PLAYER_HEIGHT = 50,90
-  PLAYER_START_X, PLAYER_START_Y =  20, (WIN.get_height()-PLAYER_HEIGHT)//2
-  ball = pygame.Rect(BALL_START_X, BALL_START_Y, BALL_LENGTH, BALL_LENGTH)
-  player_L = pygame.Rect(PLAYER_START_X, PLAYER_START_Y, PLAYER_WIDTH, PLAYER_HEIGHT)
-  player_R = pygame.Rect(WIN.get_width() - PLAYER_START_X - PLAYER_WIDTH, PLAYER_START_Y, PLAYER_WIDTH, PLAYER_HEIGHT)
+  ball,player_L,player_R=initialize()
   while running:
     clock.tick(FPS)
 
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
         running=False
+      if event.type == GAME_OVER_EVENT:
+        ball,player_L,player_R=initialize()
+
 
     keys_pressed=pygame.key.get_pressed()
     handle_mov_player_left(keys_pressed, player_L)
